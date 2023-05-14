1f49d01bb3e925b11dca9f0d489456aa2fc9c42e
gab246 <119339137+gab246@users.noreply.github.com>
2023-05-14 13:38:48 +0200

    cleaned up code

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -4,16 +4,16 @@ class CitySearch extends Component {
   state = {
     query: '',
     suggestions: []
-  }
+  } 
 
   handleInputChanged = (event) => {
     const value = event.target.value;
     const suggestions = this.props.locations.filter((location) => {
       return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
     });
-    this.setState({
-      query: value,
-      suggestions,
+    this.setState({ 
+      query: value, 
+      suggestions
     });
   };
 
@@ -26,26 +26,29 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
-        <input
+        <input 
           type="text"
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
-        />
-        <ul className="suggestions">
-          {this.state.suggestions.map((suggestion) => (
-            <li 
-            key={suggestion}
-            onClick={() => this.handleItemClicked(suggestion)}>
-              {suggestion}</li>
-          ))}
-          <li key='all'>
-            <b>See all cities</b>
-          </li>
-        </ul>
+          />
+          <ul className="suggestions">
+            {this.state.suggestions.map((suggestion) => (
+              <li 
+              key={suggestion}
+              onClick={() => this.handleItemClicked(suggestion)}>
+                {suggestion}</li>
+            ))}
+              <li key='all'>
+                <b>See all cities</b>
+              </li>
+          </ul>
       </div>
     );
   }
+  
+  
 }
+
 
 export default CitySearch;
