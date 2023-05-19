aebc505cc8370c25d58b578a3c92239479999607
gab246 <119339137+gab246@users.noreply.github.com>
2023-05-19 15:57:24 +0200

    add state, add returned elements on collapse

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 
 class Event extends Component {
+  state = {
+    hide : true,
+  } 
+
+  handleStateChange = () => {
+    this.setState((prevState) => ({
+      hide: !prevState.hide
+    }))
+   }
+
   render() {
-    return <div></div>;
+    const { event } = this.props;
+    const { hide } = this.state;
+
+    return (
+    <div className='event'>
+       <h1 className='summary'>{event.summary}</h1>
+       <p className='start'>{event.start.dateTime}</p>
+       <p className='location'>{event.location}</p>
+
+
+       {!hide && (
+        <div className='eventDetails'>
+          <h2 className='aboutTitle'>About Event:</h2>
+          <a className='eventLink' href={event.htmlLink} >See Event in Google Calendar</a>
+          <p className='description'>{event.description}</p>
+        </div>
+       )}
+
+        <button 
+        type='button'
+        className='detailsButton' 
+        onClick={() => this.handleStateChange()}>{hide ? 'Show': 'Hide'} Details
+        </button>
+      </div>
+    );
   }
 }
 export default Event;
