6993d7a5e100cf4df101b274842c5d2ae9ac3461
gab246 <119339137+gab246@users.noreply.github.com>
2023-05-28 19:32:36 +0200

    update handleInputChange

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -5,37 +5,21 @@ class NumberOfEvents extends Component {
     eventCount: 32,
   };
 
-  handleInputChange = (event) => {
-    const numInput = event.target.value;
-    this.setState ({
-      event: event,
-      eventCount: numInput,
-    })
 
-    //location is undefined
-    this.props.eventsUpdate(undefined, numInput)
+  handleInputChange = async (event) => {
+    await this.setState({ eventCount: event.target.value });
+    this.props.updateEvents(null, event.target.value);
+  }
 
-    if (numInput <1 || numInput > 32){
-      this.setState({
-        errorText: 'Try Again! Please enter a number between 1 and 32'
-      })
-     } else {
-        return this.setState({
-          errorText: ''
-        });
-      }
-    }
   
   render(){
     return (
-      <div className='container'>
+      <div className='numberOfEvents'>
         <div>Number of Events</div>
           <input
             type='number'
             className='eventNumber'
             id='num'
-            min={1}
-            max={32}
             value={this.state.eventCount}
             onChange={this.handleInputChange}
           />
