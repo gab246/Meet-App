78648f82a59153ca473225a87fb55ae2fe12ca95
gab246 <119339137+gab246@users.noreply.github.com>
2023-06-01 15:13:44 +0200

    updated input change

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,24 +2,36 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
   state = {
-    eventCount: 32,
+    eventCount: 64,
   };
 
-
   handleInputChange = async (event) => {
-    await this.setState({ eventCount: event.target.value });
-    this.props.updateEvents(null, event.target.value);
+    let value = event.target.value;
+    if(value <= 64 && value > 0){
+      this.setState({
+        eventCount: value
+      });
+
+      this.props.updateEvents(null, value);
+      
+    } else {
+      this.setState({
+        eventCount: value
+      })
+    };
+   
   }
 
-  
   render(){
     return (
-      <div className='numberOfEvents'>
+      <div className='numberOfEvents-container'>
         <div>Number of Events</div>
           <input
             type='number'
             className='eventNumber'
             id='num'
+            min={1}
+            max={64}
             value={this.state.eventCount}
             onChange={this.handleInputChange}
           />
