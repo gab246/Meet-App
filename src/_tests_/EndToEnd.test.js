6249aa55476d54850f6fb4e413100c8cf0bfe407
gab246 <119339137+gab246@users.noreply.github.com>
2023-06-06 17:31:38 +0200

    created EndToEnd test

---
+++ src/_tests_/EndToEnd.test.js
@@ -0,0 +1,40 @@
+import puppeteer from 'puppeteer';
+
+describe('show/hide details of an event', () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    jest.setTimeout(30000);
+    browser = await puppeteer.launch({
+  //  headless: false,
+  //  slowMo: 250, 
+  //  ignoreDefaultArgs: ['--disable-extensions']
+    });
+    page= await browser.newPage();
+    await page.goto('http://localhost:3000/');
+    await page.waitForSelector('.event');
+  })
+
+  afterAll(() => {
+    browser.close();
+  });
+  
+  test('An event element is collapsed by default', async () => {
+    const eventDetails = await page.$('.event .eventDetails');
+    expect(eventDetails).toBeNull();
+  })
+
+  test('user can expand an event to see its details', async () => {
+    const eventDetails = await page.$('.event .detailsButton');
+    expect(eventDetails).toBeDefined();
+  })
+
+  test('user can collapse an event to hide its details', async () => {
+    const eventDetails = await page.$('.event .eventDetails');
+    expect(eventDetails).toBeDefined();
+    await page.click('.detailsButton');
+    expect(eventDetails).toBeNull();
+  })
+});
+
+
