fa6fe60b4f958b835e4938b6a47a477053e8a292
gab246 <119339137+gab246@users.noreply.github.com>
2023-06-13 12:38:21 +0200

    styled error alert messages

--- src/Alert.js
+++ src/Alert.js
@@ -4,6 +4,7 @@ class Alert extends Component {
   constructor(props) {
     super(props);
     this.color = null;
+    this.marginTop = null;
   }
 
   getStyle = () => {
@@ -11,13 +12,17 @@ class Alert extends Component {
       color: this.color,
       fontStyle: 'italic',
       fontSize: '18px',
+      fontWeight: '550',
+      marginTop: this.marginTop,
     };
   }
 
   render() {
     return (
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
       <div className="Alert">
         <p style={this.getStyle()}>{this.props.text}</p>
+      </div>
       </div>
     );
   }
@@ -26,14 +31,16 @@ class Alert extends Component {
   class InfoAlert extends Alert {
     constructor(props) {
       super(props);
-      this.color = 'blue';
+      this.color = '#6EB7D6';
+      this.marginTop = '330px';
     }
   }
 
   class ErrorAlert extends Alert {
     constructor(props) {
       super(props);
-      this.color = 'red';
+      this.color = '#ff6961';
+      this.marginTop = '560px'
     }
   }
 
