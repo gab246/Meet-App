325e583e8222556e0526e39d4d004f9d00f47c70
gab246 <119339137+gab246@users.noreply.github.com>
2023-06-22 14:39:18 +0200

    added cells and color

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,25 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
+const EventGenre = ({ events }) => {
+  const [data, setData] = useState([]);
+  const colors = ['#CABAFO', '#FDE4CF', '#FFCFD2', '#F1COE8', '#FBF8CC'];
+
+  useEffect(() => { 
+    setData(() => getData(events)); 
+  }, [events]);
 
   const getData = (events) => {
     const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
     const data = genres.map((genre) => {
-      const value = events.filter(({ summary }) => 
-      summary.split(' ').includes(genre)).length;
+      const value = events.filter(event => event.summary.includes(genre)).length;
        
       return { name: genre, value };
     });
   
     return data;
   };
-
-  const EventGenre = ({ events }) => {
-    const [data, setData] = useState([]);
-  
-    useEffect(() => { 
-      setData(() => getData(events)); 
-    }, [events]);
 
   return (
     <ResponsiveContainer height={400} >
@@ -29,11 +28,16 @@ import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
           cx={200}
           cy={200}
           labelLine={false}
-          outRadius={80}
+          outerRadius={80}
           fill='#8884d8'
           dataKey='value'
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
           >
+            {
+               data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+               ))
+              }
 
         </Pie>
       </PieChart>
