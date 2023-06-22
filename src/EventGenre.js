7c3082360cfb4f4763f668849e43f2ccbba74c15
gab246 <119339137+gab246@users.noreply.github.com>
2023-06-22 15:06:26 +0200

    updated cell color

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -29,13 +29,12 @@ const EventGenre = ({ events }) => {
           cy={200}
           labelLine={false}
           outerRadius={80}
-          fill='#8884d8'
           dataKey='value'
           label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
           >
             {
                data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={color[index]} />
+                <Cell key={`cell-${index}`} fill={color[0]} />
                ))
               }
 
